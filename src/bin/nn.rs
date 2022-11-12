045080657a7ba105cf5c417d470c4b0065ed66a6
Miraj Shah <mirajholmes@gmail.com>
2022-11-12 17:08:30 +0530

    fixed perf issue

--- src/bin/nn.rs
+++ src/bin/nn.rs
@@ -2,9 +2,9 @@ use tinygrad_rust::{datasets::{mnist, PX_SIZE}, neural_net};
 
 fn main() {
     let mnist_dataloader = mnist::load_data();
-    let batch_size = 60;
-    let epochs = 5;
-    let mut model = neural_net::Model::init(PX_SIZE * PX_SIZE, 10, vec![8], mnist_dataloader);
+    let batch_size = 10;
+    let epochs =30;
+    let mut model = neural_net::Model::init(PX_SIZE * PX_SIZE, 10, vec![16, 16], mnist_dataloader);
 
     model.train(batch_size, epochs)
 }
